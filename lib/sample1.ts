0983768118942682cc7bc49914044b6e8eb0e070
Rozamo <35562703+Rozamo@users.noreply.github.com>
2024-02-05 21:29:02 +0530

    feat: test type name clash

--- lib/sample1.ts
+++ lib/sample1.ts
@@ -1,8 +1,8 @@
-type Sample1 = () => "sample1";
+type Sample = () => "sample1";
 
-const sample1: Sample1 = () => {
+const sample1: Sample = () => {
   return "sample1";
 };
 
 export { sample1 };
-export type { Sample1 };
+export type { Sample };
--- lib/sample2.ts
+++ lib/sample2.ts
@@ -1,8 +1,8 @@
-type Sample2 = () => "sample2";
+type Sample = () => "sample2";
 
-const sample2: Sample2 = () => {
+const sample2: Sample = () => {
   return "sample2";
 };
 
 export { sample2 };
-export type { Sample2 };
+export type { Sample };
